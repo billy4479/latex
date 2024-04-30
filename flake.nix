2f9268f3f5f2948bd66001a7daf64d36d891f2b6
billy4479 <giachi.ellero@gmail.com>
2024-04-30 16:47:50 +0200

    Add custom fonts

--- flake.nix
+++ flake.nix
@@ -12,14 +12,37 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      devShells.default = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [
-          texliveFull # TODO: This can probably be reduced
-
-          # `minted` dependencies
-          python3
-          python3Packages.pygments
+      devShells.default = let
+        fonts = with pkgs; [
+          fira-code
+          ubuntu_font_family
+          fg-virgil # excalidraw font
         ];
-      };
+
+        # https://discourse.nixos.org/t/project-local-fonts/22174/2?u=billy4479
+        # https://github.com/Leixb/latex-template/blob/274df8a3cf8641906f32eef114f8fcf2d496936e/build-document.nix#L34
+        fontDir = pkgs.symlinkJoin {
+          name = "fonts";
+          paths = fonts;
+        };
+      in
+        pkgs.mkShell {
+          nativeBuildInputs = with pkgs;
+            [
+              texliveFull # TODO: This can probably be reduced
+
+              # `minted` dependencies
+              python3
+              python3Packages.pygments
+
+              inkscape # `svg` dependencies
+            ]
+            ++ fonts;
+
+          OSFONTDIR = "${fontDir}/share/fonts";
+          FONTCONFIG_FILE = pkgs.makeFontsConf {
+            fontDirectories = fonts;
+          };
+        };
     });
 }
